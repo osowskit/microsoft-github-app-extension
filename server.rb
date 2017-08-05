c9eb694c79bc658654258882e0fc1c87ced3fe4a
Thomas Osowski <osowskit@github.com>
2017-08-04 19:56:01 -0700

    print referrer query string

--- server.rb
+++ server.rb
@@ -88,11 +88,13 @@ end
 # Entry point for VSTS Add-on.
 get '/main_entry' do
   session[:referrer] = request.referer
-  puts "request #{request.query_string.to_s}"
-  
+  puts "request #{request.query_string}"
+  puts "request #{request.fullpath}"
+  puts "referer #{request.referer}"
+  puts "request val at #{request.values_at('id')}"
   # JIRA ID is passed as context-parameters.
   # 
-  session[:work_item] = request.query_string.fetch("id", $default_branch_name)
+  #session[:work_item] = request.query_string.fetch("id", $default_branch_name)
   puts "my work item #{session[:work_item]}"
   
   redirect to('/')
